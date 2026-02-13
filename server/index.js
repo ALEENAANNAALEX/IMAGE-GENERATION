627813c283a2238c05d05e7843336789337dbc9b
Aleena <aleenaannaalex2026@mca.ajce.in>
2026-02-13 16:57:53 +0530

    Final Render fix: Express 5 wildcard and optimized build

--- server/index.js
+++ server/index.js
@@ -76,7 +76,7 @@ app.post('/api/generate', async (req, res) => {
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('/*', (req, res) => {
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
 }
